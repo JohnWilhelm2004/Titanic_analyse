c6433a89f51f4cb963d0c3f3c5d107d417a8e438
John Wilhelm <john@zey-web.de>
2026-02-04 11:16:23 +0100

    Hilfsfunktionen ii (John) und iii (Elaha)
    Hilfsfunktion ii von mir
    Hilfsfunktion ii füg ich für Ela mit rein

--- Skripte/Funktionen-R-Skript 2.R
+++ Skripte/Funktionen-R-Skript 2.R
@@ -27,8 +27,71 @@ detect_metric_columns <- function(titanic.data) {
 }
 ####################################
 
+####################################
+# Hilfsfunktion für ii
+####################################
+
+
+# Name Kategorieller Spalten
+detect_categorical_columns <- function(titanic.data) {
+  kategoriale_spalten <- c()
+  
+  for (col_name in names(titanic.data)) {
+    # Wir prüfen: Ist es ein FAKTOR, TEXT (character) oder LOGISCH (TRUE/FALSE)?
+    if (is.factor(titanic.data[[col_name]]) || is.character(titanic.data[[col_name]]) || is.logical(titanic.data[[col_name]])) {
+      kategoriale_spalten <- c(kategoriale_spalten, col_name)
+    }
+  }
+  # Gibt Array mit Namen zurück
+  return(kategoriale_spalten)
+}
+
+
+
+
+##################################
+# Hilfsfunktion für iii
+##################################
+calc_cramers_v <- function(contingency_table) {
+  # Hilfsfunktion zur Berechnung von Cramérs V
+  # Formel: sqrt(chi_sq / (n * (min(cols, rows) - 1)))
+  
+  chi_sq <- chisq.test(contingency_table, correct = FALSE)$statistic
+  n <- sum(contingency_table)
+  dims <- dim(contingency_table)
+  
+  v <- sqrt(chi_sq / (n * (min(dims) - 1)))
+  
+  return(as.numeric(v))
+}
+
+
+
+
+###################################
+#Hilfsfunktion für iv 
+###################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###################################
 #Hilfsfunktion für v) 
+###################################
 
 #Wir erstellen eine Hilfsfunktion für die Datentransformation die für
 #jede Variable durchgeführt wird 

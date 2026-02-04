563422682ac0d14719075793a214bfae314612f3
lukas-koenig-gh <Lukas.koenig2308@gmail.com>
2026-02-04 15:21:48 +0100

    Alle Funktionen Eingebaut

--- Bericht Code/analyse_main.R
+++ Bericht Code/analyse_main.R
@@ -34,10 +34,10 @@ kategorial.data <- berechne_kategoriale_statistiken(titanic.data = titanic.data)
 
 print(kategorial.data)
 
-#Was wir hier Rauslesen: 64,76 % der Menschen an Board waren Männlich 
-#Die Meisten kamen aus Southhampton mit 72.5%
+#Was wir hier Auslesen: 64,76 % der Menschen an Board waren Männlich 
+#Die Meisten kamen aus Southampton mit 72.5%
 #Der Großteil der Menschen ist gestorben mit einem Anteil von 61.62% 
-#Die Meisten an Board gehörten der Preisklasse 3 an also dem niedrigkostensegment
+#Die Meisten an Board gehörten der Preisklasse 3 an also dem niedrigsten
 
 #Jetzt testen wir ein paar Hypothesen 
 #Die Vermutung ist das die Entscheidung wer auf ein Rettungsbot kommt nach dem verfahren,
@@ -45,3 +45,45 @@ print(kategorial.data)
 #die Merkmale Sex und Survived zusammenhängen
 
 Cor.data.sex <- analyze_categorical_relation(titanic.data, "Sex", "Survived")
+
+print(Cor.data.sex$plot.data)
+
+#Hier können wir schon einige Beobachtungen machen bei Frauen wurden Prozentual gesehen
+#74.2% gerettet während bei den Männern es nur 18.89% überlebt haben. Lässt darauf schließen 
+#das die Hypothese korrekt war 
+
+#Jetzt Testen wir noch ob die Preisklasse in der man war einen unterschied machte 
+
+cor.data.pclass <- analyze_categorical_relation(titanic.data, "Pclass", "Survived")
+
+print(cor.data.pclass$plot.data)
+
+#Auch hier lassen sich wieder zusammenhänge finden in der Preisklasse 3 sind 75.76%
+#der Menschen gestorben während es in Preisklasse 2 nur noch 52.71% waren und in Klasse 1 
+# 37.037% zeigt das da wahrscheinlich ein Zusammenhang war 
+
+
+#Jetzt überprüfen wir etwas genauer, Preisklassen sind grobe richtungen, wir wollen schauen ob
+#der Ticketpreis direkt mit der Überlebenschance korrelierte 
+
+dicho.data.pclass <- cor_metr_dicho(titanic.data, "Fare", "Survived")
+
+print(dicho.data.pclass)
+
+#Die Korrelation liegt bei 0.257 und significance = "Ja" damit können wir sagen 
+#das die Preisklasse auf jeden Fall einen Zusammenhang mit der Überlebenschance hatte
+
+#Wir überprüfen auch noch ob das Alter eine Rolle spielte bei solchen dingen
+dicho.data.age <- cor_metr_dicho(titanic.data, "Age", "Survived")
+
+print(dicho.data.age)
+
+#Wir haben hier eine leicht negative Korrelation -0,089 daraus lässt sich deuten 
+#das eher Jüngere Personen das ganze Überlebt haben ob das daran liegt das Jüngere
+#Personen einfach länger durchgehalten haben wegen ihrer vitatlität oder ob sie
+#bevorzugt wurden lässt sich nicht konkret sagen
+
+#Wir verwenden Funktion 5 um einen Übersichtsplot über die Ereignesse zu Zeigen, 
+#Wie genau waren die Personengruppen die wir untersuchten verteilt und was genau haben sie gemacht 
+
+facet.plot.data <- v.visualisation(titanic.data, "Survived", "Sex", "Pclass", "Side")
